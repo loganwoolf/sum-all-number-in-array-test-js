1f54e23771e78ae49647e7edb61aaf390f75c5e1
Logan Woolf <loganwoolf@gmail.com>
2022-01-15 06:47:48 +0000

    Implement sumItems function
    Recursively sums nested arrays as if they are flat

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-function sumItems(array) {
+const sumItems = function(array, accumulator = 0) {
+
   // Sum all the numbers in the array
-}
+  for (let item of array) {
+    // base case, no nesting
+    if (!Array.isArray(item)) {
+      accumulator += item;
+      console.log(accumulator);
+    }
+    // recursion case, array is encountered
+    if (Array.isArray(item)) {
+      console.log('recursive call');
+      accumulator = sumItems(item, accumulator);
+
+    }
+
+  }
+
+  return accumulator;
+};
+console.log(
+  "Final Total: ",
+  sumItems([2,2,[[[[[[5,2]]]]],2]])
+);
+
 
 module.exports = sumItems;
